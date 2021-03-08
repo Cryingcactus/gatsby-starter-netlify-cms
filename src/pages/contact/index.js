92b6dabb675638fccca868e5149cc622344a4535
Jesse Burnley <46989113+Cryingcactus@users.noreply.github.com>
2021-03-08 14:35:43 -0500

    Removed broken form

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -50,7 +50,7 @@ const Index = () => {
                     <div className={styles.formContainer}>
                         <form
                             className={styles.form}
-                            name="contact5"
+                            name="contact"
                             method="post"
                             data-netlify="true"
                             data-netlify-honeypot="bot-field"
@@ -60,7 +60,7 @@ const Index = () => {
                             <input
                                 type="hidden"
                                 name="form-name"
-                                value="contact5"
+                                value="contact"
                             />
                             <p hidden>
                                 <label>
@@ -179,150 +179,6 @@ const Index = () => {
                                 </div>
                             </div>
 
-                            <div className={styles.field}>
-                                <button
-                                    className={[
-                                        styles.button,
-                                        styles.reversedColors,
-                                    ].join(" ")}
-                                    type="submit"
-                                >
-                                    Send
-                                </button>
-                            </div>
-                        </form>
-                        <form
-                            className={styles.form}
-                            name="contact"
-                            method="post"
-                            data-netlify="true"
-                            data-netlify-honeypot="bot-field"
-                            onSubmit={handleSubmit}
-                        >
-                            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                            <input
-                                type="hidden"
-                                name="form-name"
-                                value="contact"
-                            />
-                            <div hidden>
-                                <label>
-                                    Don’t fill this out:{" "}
-                                    <input
-                                        name="bot-field"
-                                        onChange={handleChange}
-                                    />
-                                </label>
-                            </div>
-                            <div className={[styles.field].join(" ")}>
-                                <label
-                                    className={[
-                                        styles.label,
-                                        fieldFocused === "name" ||
-                                        (state.name !== undefined &&
-                                            state.name !== "")
-                                            ? styles.fieldTitle
-                                            : styles.placeholder,
-                                    ].join(" ")}
-                                    htmlFor={"name"}
-                                >
-                                    what should we call you?
-                                </label>
-                                <div className={styles.control}>
-                                    <input
-                                        type={"text"}
-                                        name={"name"}
-                                        id={"name"}
-                                        required={true}
-                                        onChange={handleChange}
-                                        onFocus={handleFocus}
-                                        onBlur={handleUnfocus}
-                                    />
-                                </div>
-                            </div>
-                            <div className={[styles.field].join(" ")}>
-                                <label
-                                    className={[
-                                        styles.label,
-                                        fieldFocused === "company" ||
-                                        (state.company !== undefined &&
-                                            state.company !== "")
-                                            ? styles.fieldTitle
-                                            : styles.placeholder,
-                                    ].join(" ")}
-                                    htmlFor={"company"}
-                                >
-                                    where do you work?
-                                </label>
-                                <div className={styles.control}>
-                                    <input
-                                        type={"text"}
-                                        name={"company"}
-                                        id={"company"}
-                                        required={true}
-                                        onChange={handleChange}
-                                        onFocus={handleFocus}
-                                        onBlur={handleUnfocus}
-                                    />
-                                </div>
-                            </div>
-                            <div className={[styles.field].join(" ")}>
-                                <label
-                                    className={[
-                                        styles.label,
-                                        fieldFocused === "title" ||
-                                        (state.title !== undefined &&
-                                            state.title !== "")
-                                            ? styles.fieldTitle
-                                            : styles.placeholder,
-                                    ].join(" ")}
-                                    htmlFor={"title"}
-                                >
-                                    what’s your title?
-                                </label>
-                                <div className={styles.control}>
-                                    <input
-                                        type={"text"}
-                                        name={"title"}
-                                        id={"title"}
-                                        required={true}
-                                        onChange={handleChange}
-                                        onFocus={handleFocus}
-                                        onBlur={handleUnfocus}
-                                    />
-                                </div>
-                            </div>
-                            <div
-                                className={[
-                                    styles.field,
-                                    styles.textareaField,
-                                ].join(" ")}
-                            >
-                                <label
-                                    className={[
-                                        styles.label,
-                                        fieldFocused === "message" ||
-                                        (state.message !== undefined &&
-                                            state.message !== "")
-                                            ? styles.fieldTitle
-                                            : styles.placeholder,
-                                    ].join(" ")}
-                                    htmlFor={"message"}
-                                >
-                                    what’s on your mind?
-                                </label>
-                                <div className={styles.control}>
-                                    <textarea
-                                        className={styles.reversedColors}
-                                        name={"message"}
-                                        id={"message"}
-                                        required={true}
-                                        onChange={handleChange}
-                                        onFocus={handleFocus}
-                                        onBlur={handleUnfocus}
-                                    />
-                                </div>
-                            </div>
                             <div className={styles.field}>
                                 <button
                                     className={[
