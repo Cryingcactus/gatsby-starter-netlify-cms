fe6f2ac2c59d2ee9264d8596d29cd361b4d93461
Jesse Burnley <46989113+Cryingcactus@users.noreply.github.com>
2021-03-08 10:50:25 -0500

    no ajax submission

--- src/pages/contact/Form.js
+++ src/pages/contact/Form.js
@@ -38,48 +38,43 @@ const Form = ({ children }) => {
             .catch((error) => alert(error));
     };
     return (
-        <div>
-            <form
-                className={styles.form}
-                name="contact"
-                method="post"
-                data-netlify="true"
-                data-netlify-honeypot="bot-field"
-                onSubmit={handleSubmit}
-            >
-                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                <input type="hidden" name="form-name" value="contact" />
-                <div hidden>
-                    <label>
-                        Don’t fill this out:{" "}
-                        <input name="bot-field" onChange={handleChange} />
-                    </label>
-                </div>
-                {React.Children.map(children, (child) => {
-                    return React.cloneElement(child, {
-                        key: child.props.name,
-                        name: child.props.name,
-                        fieldFocused:
-                            (state[child.props.name] !== undefined &&
-                                state[child.props.name] !== "") ||
-                            fieldFocused === child.props.name,
-                        handleChange: handleChange,
-                        handleFocus: handleFocus,
-                        handleUnfocus: handleUnfocus,
-                    });
-                })}
-                <div className={styles.field}>
-                    <button
-                        className={[styles.button, styles.reversedColors].join(
-                            " "
-                        )}
-                        type="submit"
-                    >
-                        Send
-                    </button>
-                </div>
-            </form>
-        </div>
+        <form
+            className={styles.form}
+            name="contact"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+        >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <div hidden>
+                <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" onChange={handleChange} />
+                </label>
+            </div>
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {
+                    key: child.props.name,
+                    name: child.props.name,
+                    fieldFocused:
+                        (state[child.props.name] !== undefined &&
+                            state[child.props.name] !== "") ||
+                        fieldFocused === child.props.name,
+                    handleChange: handleChange,
+                    handleFocus: handleFocus,
+                    handleUnfocus: handleUnfocus,
+                });
+            })}
+            <div className={styles.field}>
+                <button
+                    className={[styles.button, styles.reversedColors].join(" ")}
+                    type="submit"
+                >
+                    Send
+                </button>
+            </div>
+        </form>
     );
 };
 
