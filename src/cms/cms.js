75e47f8051947bec0fa2065d37048cd3393e0417
Jesse Burnley <46989113+Cryingcactus@users.noreply.github.com>
2021-03-18 10:11:10 -0400

    added preview to solutions

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,11 +6,13 @@ import cloudinary from "netlify-cms-media-library-cloudinary";
 // import BlogPostPreview from "./preview-templates/BlogPostPreview";
 // import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
+import SolutionsPagePreview from "./preview-templates/SolutionsPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("solutions", SolutionsPagePreview);
 // CMS.registerPreviewTemplate("about", AboutPagePreview);
 // CMS.registerPreviewTemplate("products", ProductPagePreview);
 // CMS.registerPreviewTemplate("blog", BlogPostPreview);
