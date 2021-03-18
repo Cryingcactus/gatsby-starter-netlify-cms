00e8833af0680cabdb84dc295b4beedd4de50277
Jesse Burnley <46989113+Cryingcactus@users.noreply.github.com>
2021-03-18 17:35:34 -0400

    values page

---
+++ src/components/MeetMeSection/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./MeetMeSection.module.css";
+import ReactMarkdown from "react-markdown";
+import TitledCopy from "../TitledCopy";
+import PreviewCompatibleImage from "../PreviewCompatibleImage";
+
+const MeetMeSection = ({ section }) => {
+    const {
+        title,
+        copy,
+        picture,
+        signature,
+        primaryColor,
+        secondaryColor,
+    } = section;
+    return (
+        <section className="nonPaddingSection">
+            <div
+                className={styles.container}
+                style={{ backgroundColor: secondaryColor, color: primaryColor }}
+            >
+                <div className={styles.leftCol}>
+                    <TitledCopy
+                        title={title}
+                        copy={<ReactMarkdown>{copy}</ReactMarkdown>}
+                    />
+                    <div className={styles.rightAligned}>
+                        <PreviewCompatibleImage
+                            imageInfo={signature}
+                            style={{ height: "104px", width: "245px" }}
+                        />
+                        <div className={styles.signatureContainer}>
+                            <ReactMarkdown>{signature.name}</ReactMarkdown>
+                            <ReactMarkdown>{signature.title}</ReactMarkdown>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <PreviewCompatibleImage
+                        imageInfo={picture}
+                        style={{ height: "100%" }}
+                    />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MeetMeSection;
--- src/components/TitleCopySection/index.js
+++ src/components/TitleCopySection/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import styles from "./TitleCopySection.module.css";
 import ReactMarkdown from "react-markdown";
 
-const TitleCopySection = ({ section }) => {
+const TitleCopySection = ({ section, alignment, invserse }) => {
     const { title, copy } = section;
+    let style;
+    if (invserse) {
+        style = { ...style, flexDirection: "column-reverse" };
+    }
+    if (alignment) {
+        style = { ...style, textAlign: alignment };
+    }
     return (
         <section className="section">
-            <div className={styles.titleContainer}>
+            <div className={styles.titleContainer} style={style}>
                 <h1>{title}</h1>
                 <p className="p1">
                     <ReactMarkdown>{copy}</ReactMarkdown>
