4547ae490ecc5a4491ebeb85e616c95be1b9f4b0
Jesse Burnley <46989113+Cryingcactus@users.noreply.github.com>
2021-03-28 00:02:21 -0400

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,26 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // The property ID; the tracking code won't be generated without it
+                trackingId: "G-WRQRBRV6KD",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: true,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths: exclude: ["/preview/**", "/do-not-track/me/too/"],
+                exclude: [],
+                // Defers execution of google analytics script after page load
+                defer: false,
+                // Any additional optional fields
+                sampleRate: 5,
+                siteSpeedSampleRate: 10,
+            },
+        },
+        {
             resolve: "gatsby-plugin-react-svg",
             options: {
                 rule: {
